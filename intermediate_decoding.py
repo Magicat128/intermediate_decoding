1684bceedf3bc13e54d1a886b0101dfb9f7d8e8e
Magicat128 <48112124+Magicat128@users.noreply.github.com>
2024-09-13 10:38:20 +0800

    Add files via upload

---
+++ intermediate_decoding.py
@@ -0,0 +1,140 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+class AttnWrapper(torch.nn.Module):
+    def __init__(self, attn):
+        super().__init__()
+        self.attn = attn
+        self.activations = None
+        self.add_tensor = None
+
+    def forward(self, *args, **kwargs):
+        output = self.attn(*args, **kwargs)
+        if self.add_tensor is not None:
+            output = (output[0] + self.add_tensor,)+output[1:]
+        self.activations = output[0]
+        return output
+
+    def reset(self):
+        self.activations = None
+        self.add_tensor = None
+
+class BlockOutputWrapper(torch.nn.Module):
+    def __init__(self, block, unembed_matrix, norm):
+        super().__init__()
+        self.block = block
+        self.unembed_matrix = unembed_matrix
+        self.norm = norm
+
+        self.block.self_attn = AttnWrapper(self.block.self_attn)
+        self.post_attention_layernorm = self.block.post_attention_layernorm
+
+        self.attn_mech_output_unembedded = None
+        self.intermediate_res_unembedded = None
+        self.mlp_output_unembedded = None
+        self.block_output_unembedded = None
+
+
+    def forward(self, *args, **kwargs):
+        output = self.block(*args, **kwargs)
+        self.block_output_unembedded = self.unembed_matrix(self.norm(output[0]))
+        attn_output = self.block.self_attn.activations
+        self.attn_mech_output_unembedded = self.unembed_matrix(self.norm(attn_output))
+        device = attn_output.device
+        attn_output = attn_output.to('cpu')
+        attn_output += args[0].cpu()
+        attn_output = attn_output.to(device)
+        self.intermediate_res_unembedded = self.unembed_matrix(self.norm(attn_output))
+        mlp_output = self.block.mlp(self.post_attention_layernorm(attn_output))
+        self.mlp_output_unembedded = self.unembed_matrix(self.norm(mlp_output))
+        return output
+
+    def attn_add_tensor(self, tensor):
+        self.block.self_attn.add_tensor = tensor
+
+    def reset(self):
+        self.block.self_attn.reset()
+
+    def get_attn_activations(self):
+        return self.block.self_attn.activations
+
+class Llama7BHelper:
+    def __init__(self, model_path):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        device_count = torch.cuda.device_count()
+        max_memory = {}
+        for i in range(device_count):
+            max_memory[i] = torch.cuda.mem_get_info(i)[0]
+        bnb_config = BitsAndBytesConfig(
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=False
+        )
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, max_memory=max_memory, device_map="auto", low_cpu_mem_usage=True, torch_dtype=torch.float16)
+        for i, layer in enumerate(self.model.model.layers):
+            self.model.model.layers[i] = BlockOutputWrapper(layer, self.model.lm_head, self.model.model.norm)
+
+    def generate_text(self, prompt, max_length=100):
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        generate_ids = self.model.generate(inputs.input_ids.to(self.device), max_length=max_length)
+        #, temperature=0.4, repetition_penalty=1.18, top_p=0.9)
+        return self.tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+
+    def get_logits(self, prompt):
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        with torch.no_grad():
+          logits = self.model(inputs.input_ids.to(self.device)).logits
+          return logits
+
+    def set_add_attn_output(self, layer, add_output):
+        self.model.model.layers[layer].attn_add_tensor(add_output)
+
+    def get_attn_activations(self, layer):
+        return self.model.model.layers[layer].get_attn_activations()
+
+    def reset_all(self):
+        for layer in self.model.model.layers:
+            layer.reset()
+
+    def print_decoded_activations(self, decoded_activations, label):
+        softmaxed = torch.nn.functional.softmax(decoded_activations[0][-1], dim=-1)
+        values, indices = torch.topk(softmaxed, 10)
+        probs_percent = [int(v * 100) for v in values.tolist()]
+        tokens = self.tokenizer.batch_decode(indices.unsqueeze(-1))
+        print(label, list(zip(tokens, probs_percent)))
+
+
+    def decode_all_layers(self, text, topk=10, print_attn_mech=True, print_intermediate_res=True, print_mlp=True, print_block=True):
+        self.get_logits(text)
+        for i, layer in enumerate(self.model.model.layers):
+            print(f'Layer {i}: Decoded intermediate outputs')
+            if print_attn_mech:
+                self.print_decoded_activations(layer.attn_mech_output_unembedded, 'Attention mechanism')
+            if print_intermediate_res:
+                self.print_decoded_activations(layer.intermediate_res_unembedded, 'Intermediate residual stream')
+            if print_mlp:
+                self.print_decoded_activations(layer.mlp_output_unembedded, 'MLP output')
+            if print_block:
+                self.print_decoded_activations(layer.block_output_unembedded, 'Block output')
+
+if __name__ == '__main__':
+    model_path = "llama-2-7b-chat-hf"
+    model = Llama7BHelper(model_path)
+    perturbation_token = 'division, /, divide'
+
+    prompt = "Q: Hannah has a mental breakdown while studying for finals and starts smashing windows in the high school's parking lot. She smashes a quarter of the students' cars' windows and 3/4ths of the teachers' cars' windows. If there are 64 students' cars with four windows each and 32 teachers' cars with two windows each, how many windows does Hannah smash?\nA: Let's think step by step. "
+    model.decode_all_layers(prompt, print_intermediate_res=True, print_mlp=True, print_block=True)
+
+    if perturbation_token:
+        print('Perturbation ...')
+        model.reset_all()
+        layer = 14
+        model.get_logits(perturbation_token)
+        attn = model.get_attn_activations(layer)
+        last_token_attn = attn[0][-1]
+        model.set_add_attn_output(layer, last_token_attn * 0.7)
+
+    print('***', model.generate_text(prompt, max_length=500))
